fdc51707a0768f1e88a06cc76015c7d286d02cd5
sycatsce <brun.emmanuel@hotmail.com>
2019-08-31 00:37:58 +0200

    added logo

--- src/features/Home/pages/HomePage.js
+++ src/features/Home/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../../assets/style/App.css'
+import logo from '../../../assets/images/logo.png';
 import SignIn from '../components/SignIn';
 
 function Home() {
   return (
     <div className="container">
-      <div className="left-block">
-        <div className="left-title"> Dimtech </div>
+      <div className="left-block">:
+        <div className="left-title"> <img src={logo} alt="logo" /> </div>
         <div className="left-text">
           <p className="left-subtitle"> Hi, <br /> Welcome on Dimtech </p>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
